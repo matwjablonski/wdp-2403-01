5459e03754458a0cf16509667c664fd61eaf710d
Piotr Sowa <piotrsowa87@gmail.com>
2024-02-22 23:42:05 +0100

    Dodanie obslugi eventu swipe

--- src/components/common/Swipeable/Swipeable.js
+++ src/components/common/Swipeable/Swipeable.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import { useSwipeable } from 'react-swipeable';
 
-const Swipeable = ({ children }) => {
+const Swipeable = ({ children, action }) => {
   const handlers = useSwipeable({
-    onSwiped: eventData => console.log('ufsowa User Swiped!', eventData),
-    onSwipedLeft: () => console.log('Swiped left!'),
-    onSwipedRight: () => console.log('Swiped right!'),
+    onSwipedLeft: () => action('increment'),
+    onSwipedRight: () => action('decrement'),
   });
+
   return <div {...handlers}>{children}</div>;
 };
 
 Swipeable.propTypes = {
   children: PropTypes.node,
+  action: PropTypes.func,
 };
 
 export default Swipeable;
--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -5,15 +5,11 @@ import styles from './Homepage.module.scss';
 
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
-import Swipeable from '../../common/Swipeable/Swipeable';
 
 const Homepage = () => (
   <div className={styles.root}>
-    <Swipeable>Swipe me</Swipeable>
     <FeatureBoxes />
-    <Swipeable>
-      <NewFurniture />
-    </Swipeable>
+    <NewFurniture />
   </div>
 );
 
