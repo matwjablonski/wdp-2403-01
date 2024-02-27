b76cb5ddfc32173481a3d128f9dd7856d47f39fb
Piotr Sowa <piotrsowa87@gmail.com>
2024-02-27 23:10:33 +0100

    [WDP-25] Style propotion content

--- src/components/features/Promotions/Promotions.js
+++ src/components/features/Promotions/Promotions.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { getAll } from '../../../redux/promotionsRedux';
-import ImageWrapper from '../../common/ImageWrapper/ImageWrapper';
 import styles from './Promotions.module.scss';
 import { clsx } from 'clsx';
 
@@ -22,9 +21,7 @@ const Promotions = () => {
   };
   const rightUpperPanelImageProps = {
     alt: panelType.rightUpper,
-    filePath: `${srcDir}${
-      promotions.find(item => item.type === panelType.rightUpper).filePath
-    }`,
+    filePath: '', //`${srcDir}${promotions.find(item => item.type === panelType.rightUpper).filePath}`,
   };
   const rightBottomPanelImageProps = {
     alt: panelType.rightBottom,
@@ -53,29 +50,28 @@ const Promotions = () => {
             <div className={clsx('d-flex flex-column', styles.rightPanel)}>
               <div className={clsx('', styles.rightUpper)}>
                 <div
-                  className={clsx(styles.bkgImage)}
+                  className={clsx('d-flex justify-content-end', styles.bkgImage)}
                   style={{
                     backgroundImage: `url(${rightUpperPanelImageProps.filePath})`,
                   }}
                 >
                   <div className={styles.content}>
-                    <p>Guest room</p>
-                    <p>Sofa</p>
-                    <p>-20%</p>
+                    <p>Office chair</p>
+                    <p>Collection</p>
+                    <p>$200.00</p>
                   </div>
                 </div>
               </div>
               <div className={clsx('', styles.rightBottom)}>
                 <div
-                  className={clsx(styles.bkgImage)}
+                  className={clsx('d-flex justify-content-end', styles.bkgImage)}
                   style={{
                     backgroundImage: `url(${rightBottomPanelImageProps.filePath})`,
                   }}
                 >
                   <div className={styles.content}>
-                    <p>Guest room</p>
-                    <p>Sofa</p>
-                    <p>-20%</p>
+                    <p>Special collection</p>
+                    <p>Save up 45% of furniture</p>
                   </div>
                 </div>
               </div>
