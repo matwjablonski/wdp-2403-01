a949a98dc495f432674c898fab9d0a1d247b4539
Przemysław Bulanda <devprzemas@gmail.com>
2024-02-29 17:15:40 +0100

    Change code, add CompanyClaimMedia

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -2,44 +2,26 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 import styles from './CompanyClaim.module.scss';
-
+import MobileCompanyClaim, {
+  cart,
+  logo,
+  phoneNumber,
+} from '../CompanyClaimMedia/CompanyClaimMedia';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMobileAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 
-const CompanyClaim = () => (
-  <div className={styles.root}>
-    <div className='container'>
-      {/* DESKTOP */}
-      <div className={`row align-items-center ${styles.desktop}`}>
-        <div className={`col text-left ${styles.phoneNumber}`}>
-          <p>
-            <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
-            222
-          </p>
-        </div>
-        <div className='col text-center'>
-          <a href='#'>
-            <img src='/images/logo.png' alt='Bazar' />
-          </a>
-        </div>
-        <div className={`col text-right ${styles.cart}`}>
-          <a href='#' className={styles.cartBox}>
-            <div className={styles.cartIcon}>
-              <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
-            </div>
-            <div className={styles.cartCounter}>0</div>
-          </a>
-        </div>
-      </div>
-      {/*Mobile */}
-      <div className={`row align-items-center ${styles.mobile}`}>
-        <div className={`col text-center ${styles.logo}`}>
-          <a href='/'>
-            <img src='/images/logo.png' alt='Bazar' />
-          </a>
-        </div>
-        <div className={`col mt-3`}>
-          <div className={`col text-center ${styles.cart}`}>
+const CompanyClaim = () => {
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className={`row align-items-center ${styles.desktop}`}>
+          <div className={`col text-left ${styles.phoneNumber}`}>{phoneNumber}</div>
+          <div className='col text-center'>
+            <a href='#'>
+              <img src='/images/logo.png' alt='Bazar' />
+            </a>
+          </div>
+          <div className={`col text-right ${styles.cart}`}>
             <a href='#' className={styles.cartBox}>
               <div className={styles.cartIcon}>
                 <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
@@ -47,17 +29,19 @@ const CompanyClaim = () => (
               <div className={styles.cartCounter}>0</div>
             </a>
           </div>
-          <div className={`col text-center ${styles.phoneNumber}`}>
-            <p>
-              <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560
-              - 222
-            </p>
-          </div>
         </div>
+
+        <MobileCompanyClaim>
+          {logo}
+          <div className={`col mt-3`}>
+            {cart}
+            <div className={`col text-center ${styles.phoneNumber}`}>{phoneNumber}</div>
+          </div>
+        </MobileCompanyClaim>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 // CompanyClaim.propTypes = {};
 
---
+++ src/components/layout/CompanyClaimMedia/CompanyClaimMedia.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMobileAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
+
+import styles from '../CompanyClaim/CompanyClaim.module.scss';
+
+export const logo = (
+  <div className={`col text-center ${styles.logo}`}>
+    <a href='/'>
+      <img src='/images/logo.png' alt='Bazar' />
+    </a>
+  </div>
+);
+
+export const cart = (
+  <div className={`col text-center ${styles.cart}`}>
+    <a href='#' className={styles.cartBox}>
+      <div className={styles.cartIcon}>
+        <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
+      </div>
+      <div className={styles.cartCounter}>0</div>
+    </a>
+  </div>
+);
+export const phoneNumber = (
+  <p>
+    <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 - 222
+  </p>
+);
+const MobileCompanyClaim = props => {
+  return (
+    <div className={`row align-items-center ${styles.mobile}`}>{props.children}</div>
+  );
+};
+export default MobileCompanyClaim;
+
+MobileCompanyClaim.propTypes = {
+  children: PropTypes.node,
+};
