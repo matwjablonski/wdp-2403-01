03f2f8ce7ca3214cb85b68aa572ae741e2f8a442
Julia Nowak <julianowak077@gmail.com>
2024-03-04 16:48:47 +0100

    fix

--- src/components/common/Price/Price.js
+++ src/components/common/Price/Price.js
@@ -5,7 +5,6 @@ import styles from './Price.module.scss';
 const Price = ({ price, variant, className: priceBtn }) => {
   const classes = ['main'];
 
-  if (priceBtn) classes.push(styles[priceBtn]);
   if (variant) classes.push(styles[variant]);
   if (!price || price < 0) return null;
   return <div className={classes.join(' ')}>$ {price}</div>;
