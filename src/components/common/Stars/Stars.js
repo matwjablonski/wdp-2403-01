299b5c27b60929ca4e2737a1dfa4989deb71ffba
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2024-03-07 14:35:09 +0100

    Add changing color of selected stars

--- src/components/common/Stars/Stars.js
+++ src/components/common/Stars/Stars.js
@@ -4,16 +4,53 @@ import styles from './Stars.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 const Stars = props => {
+  let [filledStarsNum, setFilledStarsNum] = useState(props.stars);
+  let [onMouseEnterFlag, setOnMouseEnterFlag] = useState(false);
+  const fillSelectedStars = e => {
+    let id = e.target.getAttribute('id');
+    onMouseEnterFlag = true;
+    setOnMouseEnterFlag(onMouseEnterFlag);
+    if (id === null) {
+      id = 2;
+    }
+    filledStarsNum = parseInt(id);
+    setFilledStarsNum(filledStarsNum);
+  };
+  const unfillSelectedStars = stars => {
+    filledStarsNum = stars;
+    setFilledStarsNum(filledStarsNum);
+    onMouseEnterFlag = false;
+    setOnMouseEnterFlag(onMouseEnterFlag);
+  };
+
   return (
     <div className={styles.stars}>
       {[1, 2, 3, 4, 5].map(i => (
         <a key={i} href='#'>
-          {i <= props.stars ? (
-            <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+          {i <= filledStarsNum ? (
+            <FontAwesomeIcon
+              className={clsx(onMouseEnterFlag && styles.colorfulStarOnHover)}
+              icon={faStar}
+              id={i}
+              onMouseEnter={e => fillSelectedStars(e)}
+              onMouseLeave={() => unfillSelectedStars(props.stars)}
+            >
+              {i} stars
+            </FontAwesomeIcon>
           ) : (
-            <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+            <FontAwesomeIcon
+              className={styles.noHoverEff}
+              icon={farStar}
+              id={i}
+              onMouseEnter={e => fillSelectedStars(e)}
+              onMouseLeave={() => unfillSelectedStars(props.stars)}
+            >
+              {i} stars
+            </FontAwesomeIcon>
           )}
         </a>
       ))}
