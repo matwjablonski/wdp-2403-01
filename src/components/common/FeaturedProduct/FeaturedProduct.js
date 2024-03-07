18dd92d66afa7662a5bdeb01f9563bb00c8a08b1
basl3r <ajgrociak@gmail.com>
2024-03-07 22:50:20 +0100

    Add slider functionality

--- src/components/common/FeaturedProduct/FeaturedProduct.js
+++ src/components/common/FeaturedProduct/FeaturedProduct.js
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './FeaturedProduct.module.scss';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const FeaturedProduct = () => {
+  const slider = useRef(null);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    pauseOnHover: false,
+    swipe: false,
+    arrows: false,
+  };
+
+  const handlePrev = () => {
+    if (slider.current) {
+      slider.current.slickPrev();
+    }
+  };
+
+  const handleNext = () => {
+    if (slider.current) {
+      slider.current.slickNext();
+    }
+  };
+
   return (
     <div className={'col-9 ' + styles.promoSale}>
-      <img
-        alt='name'
-        src={`${process.env.PUBLIC_URL}/images/products/featured/featured-2.jpg`}
-      ></img>
+      <div className={styles.sliderContainer}>
+        <Slider ref={slider} {...settings}>
+          <div>
+            <img
+              alt='name'
+              src={`${process.env.PUBLIC_URL}/images/products/featured/featured-1.jpg`}
+              className={styles.sliderImage}
+            />
+          </div>
+          <div>
+            <img
+              alt='name'
+              src={`${process.env.PUBLIC_URL}/images/products/featured/featured-2.jpg`}
+              className={styles.sliderImage}
+            />
+          </div>
+          <div>
+            <img
+              alt='name'
+              src={`${process.env.PUBLIC_URL}/images/products/featured/featured-3.jpg`}
+              className={styles.sliderImage}
+            />
+          </div>
+        </Slider>
+      </div>
       <div className={styles.verticalBar}>
         <span>
           INDOOR <span> FURNITURE</span>
@@ -18,10 +67,10 @@ const FeaturedProduct = () => {
       </div>
       <div className={styles.shopNow}>SHOP NOW</div>
       <div className={styles.buttonContainer}>
-        <button className='col-6'>
+        <button className='col-6' onClick={handlePrev}>
           <FontAwesomeIcon icon={faChevronLeft} className={styles.fa} />
         </button>
-        <button className='col-6'>
+        <button className='col-6' onClick={handleNext}>
           <FontAwesomeIcon icon={faChevronRight} className={styles.fa} />
         </button>
       </div>
--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -87,7 +87,7 @@ class NewFurniture extends React.Component {
         <li key={i}>
           <a
             onClick={() => this.handlePageChange(i)}
-            className={i === activePage && styles.active}
+            className={i === activePage ? styles.active : ''}
           >
             page {i}
           </a>
@@ -108,7 +108,7 @@ class NewFurniture extends React.Component {
                   {categories.map(item => (
                     <li key={item.id}>
                       <a
-                        className={item.id === activeCategory && styles.active}
+                        className={item.id === activeCategory ? styles.active : ''}
                         onClick={() => this.handleCategoryChange(item.id)}
                       >
                         {item.name}
